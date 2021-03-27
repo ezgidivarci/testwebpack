15cf9296b7219e114d7d23b56aba4988aecc730c
ezgidivarci <60501258+ezgidivarci@users.noreply.github.com>
2021-03-27 23:05:59 +0300

    Add files via upload

---
+++ src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>WebPack and Babel Course</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
